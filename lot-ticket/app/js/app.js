5b50b2b4b6e44ceca2f4e1c2381ab8e7885fc2bb
Makac <matswidegren@gmail.com>
2016-10-26 21:59:54 +0200

    Powerball added

--- lot-ticket/app/js/app.js
+++ lot-ticket/app/js/app.js
@@ -17,29 +17,29 @@ ticketApp.config(['$routeProvider', '$mdIconProvider', '$mdThemingProvider',
 getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var highestNumber = 69;
+var defNumSelected = 5;
 
 generateTicket = function (){
 	console.log(">generateTicket");
-	var ret = [];
+	var nums = [];
 	//var numbers = [];
-	var ticketLength = 25;
-	var highestNumber = 69;
-	var defNumSelected = 6;
+	
 	for (var i = 1; i<=highestNumber; i++){
 		//numbers.push(i);
-		ret.push({number: i, selected:false});
+		nums.push({number: i, selected:false});
 	}
 	// for (var i = 0; i< 25; i++){
 	// 	var num = numbers.splice(getRandomInt(0, numbers.length-1), 1)[0];
 	// 	console.log ("numbers:" + numbers);
 	// 	console.log ("num:" + num)
-	// 	ret.push({number: num, selected:false});
+	// 	nums.push({number: num, selected:false});
 	// }
 	var numSelected = 0;
 	while (numSelected < defNumSelected){
-		var ix=getRandomInt(0, ret.length-1)
-		if (!ret[ix].selected){
-			ret[ix].selected = true;
+		var ix=getRandomInt(0, nums.length-1)
+		if (!nums[ix].selected){
+			nums[ix].selected = true;
 			numSelected++;
 		}
 	}
@@ -52,6 +52,7 @@ generateTicket = function (){
 		var r += "]";
 		return r;
 	}*/
+	var ret = {numbers: nums, powerBall: getRandomInt(1, highestNumber)}
 	console.log("<generateTicket: ret=" + ret)
 	return ret;
 }
@@ -59,6 +60,12 @@ var tickets = [generateTicket(), generateTicket()];
 ticketApp.controller('TicketController', function TicketController($scope, $routeParams) {
 	
 	console.log ("$routeParams=" + $routeParams)
+	this.minSelected = 5; // lowest allowed number of selected numbers
+	this.maxSelected = 20; // highest allowed number of selected numbers
+	this.numbers = [];
+	for (var i = 1; i<=highestNumber;i++){
+		this.numbers.push(i);
+	}
 	this.addTicket = function(){
 		this.tickets.push(generateTicket())
 	}
@@ -69,6 +76,19 @@ ticketApp.controller('TicketController', function TicketController($scope, $rout
 	this.getSelectedTicket = function(){
 		console.log(">getSelectedTicket: $routeParams.ticketId=" + $routeParams.ticketId);
 		return this.tickets[$routeParams.ticketId];
+	}
+	this.canSelect = function(){
+		return (this.getNumSelected < this.maxSelected);
+	}
+	this.getNumSelected = function(){
+		var ret = 0;
+		var t = getSelectedTicket();
+		for (var i = 0; i<t.length; i++){
+			if (t[i].selected){
+				ret++;
+			}
+		}
+		return ret;
 	}
 	this.tickets = tickets;
 
