cb8dc5af01416441b128ad3bc0c7d599a91087ee
Makac <matswidegren@gmail.com>
2016-11-15 13:55:43 +0100

    Relative paths, first attempt

--- lot-ticket/app/js/app.js
+++ lot-ticket/app/js/app.js
@@ -1,74 +1,86 @@
-var ticketApp = angular.module('ticketApp', [ 'ngRoute', 'ngMaterial']);
+var ticketApp = angular.module('ticketApp', ['ngRoute', 'ngMaterial']);
 var highestNumber = 69;
 var powerBallHighestNumber = 26;
 var defNumSelected = 5;
 
 ticketApp.config(['$routeProvider', '$mdIconProvider', '$mdThemingProvider',
-    function($routeProvider, $mdIconProvider, $mdThemingProvider)
-    {
-    	
-		$routeProvider.when('/', {
-			templateUrl: 'partials/tickets.html'
-		});
-		$routeProvider.when('/edit-ticket/', {
-			templateUrl: 'partials/edit-ticket.html'
-		});
-		$routeProvider.otherwise({redirectTo: '/'});
-    }
-]);
+		function ($routeProvider, $mdIconProvider, $mdThemingProvider) {
 
-function getRandomInt (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+			$routeProvider.when('/', {
+				templateUrl : 'partials/tickets.html'
+			});
+			$routeProvider.when('/edit-ticket/', {
+				templateUrl : 'partials/edit-ticket.html'
+			});
+			$routeProvider.otherwise({
+				redirectTo : '/'
+			});
+		}
+	]);
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-function generateTicket(quickPick){
+function generateTicket(quickPick) {
 	//console.log(">generateTicket");
 	var nums = [];
-	if (quickPick==null){
+	if (quickPick == null) {
 		quickPick = true;
 	}
-	for (var i = 1; i<=highestNumber; i++){
+	for (var i = 1; i <= highestNumber; i++) {
 		//numbers.push(i);
-		nums.push({number: i, selected:false});
+		nums.push({
+			number : i,
+			selected : false
+		});
 	}
 	var numSelected = 0;
 	var pb = '';
-	if(quickPick){
-		while (numSelected < defNumSelected){
-		var ix=getRandomInt(0, nums.length-1)
-			if (!nums[ix].selected){
-				nums[ix].selected = true;
-				numSelected++;
-			}
+	if (quickPick) {
+		while (numSelected < defNumSelected) {
+			var ix = getRandomInt(0, nums.length - 1)
+				if (!nums[ix].selected) {
+					nums[ix].selected = true;
+					numSelected++;
+				}
 		}
 		pb = getRandomInt(1, powerBallHighestNumber)
 	}
 
-	var ret = {numbers: nums, powerBall: pb}
+	var ret = {
+		numbers : nums,
+		powerBall : pb
+	}
 	//console.log("<generateTicket: ret=" + ret)
 	return ret;
 }
-function copyTicket (ticket){
+function copyTicket(ticket) {
 	//console.log (">copyTicket: ticket=" + ticketToString(ticket));
 	var nums = [];
-	for (var i = 0; i<ticket.numbers.length; i++){
+	for (var i = 0; i < ticket.numbers.length; i++) {
 		var org = ticket.numbers[i];
-		nums.push({number: org.number, selected: org.selected})
+		nums.push({
+			number : org.number,
+			selected : org.selected
+		})
 	}
-	var ret = {numbers: nums, powerBall: ticket.powerBall}
+	var ret = {
+		numbers : nums,
+		powerBall : ticket.powerBall
+	}
 	//console.log ("<copyTicket: ret=" + ticketToString(ret));
 	return ret;
 }
-function ticketToString(ticket){
+function ticketToString(ticket) {
 	var ret = "null";
-	if (ticket){
-		ret="{ numbers: [";
-		for (var i = 0; i<ticket.numbers.length; i++){
+	if (ticket) {
+		ret = "{ numbers: [";
+		for (var i = 0; i < ticket.numbers.length; i++) {
 			var org = ticket.numbers[i];
-			ret +="{number: " + org.number + ", selected: " + org.selected+ "}"
-			if (i<ticket.numbers.length-1){
-				ret +=", ";
+			ret += "{number: " + org.number + ", selected: " + org.selected + "}"
+			if (i < ticket.numbers.length - 1) {
+				ret += ", ";
 			}
 		}
 		ret += ", powerBall: " + ticket.powerBall;
@@ -84,13 +96,23 @@ function formatCurrency(amount, c, d, t) {
 	s = n < 0 ? "-" : "",
 	i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
 	j = (j = i.length) > 3 ? j % 3 : 0;
-	var ret =  "£" + s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+	var ret = "£" + s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 	//console.log("<formatCurrency: ret=" + ret);
 	return ret;
 };
-ticketApp.controller('TicketController', function TicketController($scope, $routeParams, $location, $window) {
-	
-	//console.log ("$routeParams=" + $routeParams)
+
+ticketApp.controller('TicketController', function TicketController($scope, $routeParams, $rootScope, $route) {
+
+	//console.log ("$routeParams=" + JSON.stringify($routeParams));
+	console.log("$route=" + JSON.stringify($routeParams));
+	var self = this;
+	self.routeChangeSuccess = $rootScope.$on("$routeChangeSuccess",
+			function (event, next, current) {
+			//read params here
+			//this.skin = $route.params.skin
+			self.routeChangeSuccess(); //this will destroy the function
+		});
+
 	this.minSelected = 5; // lowest allowed number of selected numbers
 	this.maxSelected = 20; // highest allowed number of selected numbers
 	this.maxTickets = 6;
@@ -108,50 +130,96 @@ ticketApp.controller('TicketController', function TicketController($scope, $rout
 	this.powerBalls = [];
 	this.editTicketId = null;
 	this.newTicket = false;
+
 	this.systems = {
-		s6: {name:"System 06", numLines: 6},
-		s7: {name:"System 07", numLines: 21},
-		s8: {name:"System 08", numLines: 56},
-		s9: {name:"System 09", numLines: 126},
-		s10: {name:"System 10", numLines: 252},
-		s11: {name:"System 11", numLines: 462},
-		s12: {name:"System 12", numLines: 792},
-		s13: {name:"System 13", numLines: 1287},
-		s14: {name:"System 14", numLines: 2002},
-		s15: {name:"System 15", numLines: 3003},
-		s16: {name:"System 16", numLines: 4368},
-		s17: {name:"System 17", numLines: 6188},
-		s18: {name:"System 18", numLines: 8568},
-		s19: {name:"System 19", numLines: 11628},
-		s20: {name:"System 20", numLines: 15504}
+		s6 : {
+			name : "System 06",
+			numLines : 6
+		},
+		s7 : {
+			name : "System 07",
+			numLines : 21
+		},
+		s8 : {
+			name : "System 08",
+			numLines : 56
+		},
+		s9 : {
+			name : "System 09",
+			numLines : 126
+		},
+		s10 : {
+			name : "System 10",
+			numLines : 252
+		},
+		s11 : {
+			name : "System 11",
+			numLines : 462
+		},
+		s12 : {
+			name : "System 12",
+			numLines : 792
+		},
+		s13 : {
+			name : "System 13",
+			numLines : 1287
+		},
+		s14 : {
+			name : "System 14",
+			numLines : 2002
+		},
+		s15 : {
+			name : "System 15",
+			numLines : 3003
+		},
+		s16 : {
+			name : "System 16",
+			numLines : 4368
+		},
+		s17 : {
+			name : "System 17",
+			numLines : 6188
+		},
+		s18 : {
+			name : "System 18",
+			numLines : 8568
+		},
+		s19 : {
+			name : "System 19",
+			numLines : 11628
+		},
+		s20 : {
+			name : "System 20",
+			numLines : 15504
+		}
 	};
-	for (var i = 1; i<=highestNumber;i++){
+	for (var i = 1; i <= highestNumber; i++) {
 		this.numbers.push(i);
 	}
-	for (var i = 1; i<=this.highestPowerBall;i++){
+	for (var i = 1; i <= this.highestPowerBall; i++) {
 		this.powerBalls.push(i);
 	}
-	this.update = function(){
+	this.update = function () {
 		this.canAddTicket = this.tickets.length < this.maxTickets;
-		
+
 	}
-	this.addTicket = function(quickPick){
+	this.addTicket = function (quickPick) {
 		this.tickets.push(generateTicket(quickPick));
 		this.update();
-		if (!quickPick){
-			this.editTicket(this.tickets.length-1)
+		if (!quickPick) {
+			this.editTicket(this.tickets.length - 1)
 			this.newTicket = true;
 		}
 	}
-	this.getStake = function(){
+	this.getStake = function () {
 		var p = 0;
 		var pp = 0;
-		if (this.powerPlay){
+		if (this.powerPlay) {
 			pp = this.powerPlayLinePrice;
 		}
-		for (var i = 0; i<this.tickets.length; i++){
+		for (var i = 0; i < this.tickets.length; i++) {
 			var sys = this.getSystem(this.tickets[i]);
-			if (sys != null){
+			if (sys != null) {
 				var tp = this.getSystem(this.tickets[i]).numLines * (this.linePrice + pp);
 				//console.log("tp=" + tp);
 				p += tp;
@@ -160,69 +228,69 @@ ticketApp.controller('TicketController', function TicketController($scope, $rout
 			}
 		}
 		var daysPerWeek = 1;
-		if (this.selectedDraw == "Tue & Thu"){
+		if (this.selectedDraw == "Tue & Thu") {
 			daysPerWeek = 2;
 		}
 		p = p * Number(this.duration) * daysPerWeek;
 		return formatCurrency(p, 2);
 	}
-	this.deleteTicket = function(index){
-		
+	this.deleteTicket = function (index) {
+
 		this.tickets.splice(index, 1);
 		this.update();
 	}
-	this.editTicket = function(ticketId){
+	this.editTicket = function (ticketId) {
 		//console.log(">editTicket: ticketId=" + ticketId);
 		ticketEdit = copyTicket(this.tickets[ticketId]);
 		this.editTicketId = ticketId;
 		$location.path('/edit-ticket');
 		//console.log("<editTicket; ticketEdit=" + ticketToString(ticketEdit));
 	}
-	this.getSelectedTicket = function(){
+	this.getSelectedTicket = function () {
 		//console.log(">getSelectedTicket; ticketEdit=" + ticketEdit);
 		//console.log("<getSelectedTicket; ticketEdit=" + ticketEdit);
 		return ticketEdit;
 	}
-	this.canSelect = function(ticket){
+	this.canSelect = function (ticket) {
 		//console.log(">this.canSelect:ticket=" + this.canSelect);
-		if (ticket){
+		if (ticket) {
 			return (this.getNumSelected(ticket) < this.maxSelected);
 		} else {
 			return false;
 		}
 	}
-	this.commitTicket= function (){
+	this.commitTicket = function () {
 		//console.log(">commitTicket");
 		this.tickets[this.editTicketId] = ticketEdit;
 		//console.log("this.tickets["+this.editTicketId+"]=" + ticketToString(this.tickets[this.editTicketId]));
-		
+
 		ticketEdit = null;
 		this.editTicketId = null;
 		this.newTicket = false;
-		
+
 		//console.log("<commitTicket: this.tickets=" + this.tickets);
 		$location.path('"#/tickets"');
 	}
-	this.cancelTicketChanges = function(){
+	this.cancelTicketChanges = function () {
 		//console.log(">cancelTicketChanges");
 		//console.log(">cancelTicketChanges");
 		$location.path('"#/tickets"');
-		if (this.newTicket){
+		if (this.newTicket) {
 			this.deleteTicket(this.editTicketId)
 		}
 		ticketEdit = null;
 		this.editTicketId = null;
 		this.newTicket = false;
-		
+
 		//console.log("<cancelTicketChanges: ticketEdit=" + ticketEdit);
 	}
-	this.getNumSelected = function(ticket){
+	this.getNumSelected = function (ticket) {
 		//console.log(">getNumSelected: ticket=" + ticketToString(ticket));
 		var ret = 0;
-		if (ticket){
+		if (ticket) {
 			var t = ticket;
-			for (var i = 0; i<t.numbers.length; i++){
-				if (t.numbers[i].selected){
+			for (var i = 0; i < t.numbers.length; i++) {
+				if (t.numbers[i].selected) {
 					ret++;
 				}
 			}
@@ -230,21 +298,21 @@ ticketApp.controller('TicketController', function TicketController($scope, $rout
 		//console.log("<getNumSelected: ret=" + ret);
 		return ret;
 	}
-	this.getSystem = function(ticket){
+	this.getSystem = function (ticket) {
 		//console.log(">this.getSystem: ticket=" + ticket);
-		var ret=null;
-		if (ticket){
+		var ret = null;
+		if (ticket) {
 			var numSelected = this.getNumSelected(ticket);
 			//var name = "0" + numSelected.toString();
 			//var numLines = 1 + Math.pow(this.defNumSelected, numSelected-this.defNumSelected);
 			ret = this.systems["s" + numSelected.toString()];
-			
+
 		}
 		//console.log("<this.getSystem: ret=" + ret);
 		return ret;
 		//return {name: name, numLines: numLines};
 	}
-	this.getEmptySlotsArray = function(ticket){
+	this.getEmptySlotsArray = function (ticket) {
 		//console.log(">getEmptySlotsArray: ticket=" + ticket);
 		var numSelected = this.getNumSelected(ticket);
 		//console.log ("numSelected=" + numSelected);
@@ -255,19 +323,19 @@ ticketApp.controller('TicketController', function TicketController($scope, $rout
 		//console.log("<getEmptySlotsArray:ret=" + ret)
 		return ret;
 	}
-	this.getFirstDrawDate = function(){
+	this.getFirstDrawDate = function () {
 		var now = new Date();
 		var dow = -1;
 		var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-		switch (this.selectedDraw){
-			case "Tuesday & Thursday":
+		switch (this.selectedDraw) {
+		case "Tuesday & Thursday":
 			dow = 2;
 			break;
-			case "Tuesday":
+		case "Tuesday":
 			dow = 2;
 			break;
-			case "Thursday":
+		case "Thursday":
 			dow = 4;
 			break;
 		}
@@ -279,82 +347,72 @@ ticketApp.controller('TicketController', function TicketController($scope, $rout
 
 		return dd + '/' + monthNames[mm] + '/' + yy;
 	}
-	this.getNextWeekDay = function(d, dow){
+	this.getNextWeekDay = function (d, dow) {
 		//console.log(">getNextWeekDay d=" + d + ", dow=" + dow);
 		//Given a date d and a dow (day of week, 0 - 6, where 0 is sunday, returns the next date (including the current one) that the given weekday occurs)
-	    d.setDate(d.getDate() + (dow+(7-d.getDay())) % 7);
-	    //console.log("<getNextWeekDay d=" + d);
-	    return d;
+		d.setDate(d.getDate() + (dow + (7 - d.getDay())) % 7);
+		//console.log("<getNextWeekDay d=" + d);
+		return d;
 	}
 
-	this.selectCell = function(cell)
-	{
+	this.selectCell = function (cell) {
 		cell.selected = !cell.selected;
 	}
 
-	this.selectDraw = function(num)
-	{
+	this.selectDraw = function (num) {
 		//console.log("kamagong numero >>> ", num);
 
 		this.selectedDraw = num;
 	}
 
-	this.selectPowerBall = function(num)
-	{
+	this.selectPowerBall = function (num) {
 		//console.log("Manoling >>> ", num);
 
 		this.getSelectedTicket().powerBall = num;
 	}
 
-	this.selectDuration = function(duration)
-	{
+	this.selectDuration = function (duration) {
 		//console.log("hari ng stunt >>> ", duration);
 
 		this.duration = duration;
-		if (this.duration == 1){
+		if (this.duration == 1) {
 			this.durationString = "1 week"
 		} else {
-			this.durationString =  this.duration.toString() + " weeks"
+			this.durationString = this.duration.toString() + " weeks"
 		}
 	}
 
 	this.durationString = "1 week";
-	this.draws=["Tue", "Thu", "Tue & Thu"];
-	this.selectedDraw= "Tue & Thu";
+	this.draws = ["Tue", "Thu", "Tue & Thu"];
+	this.selectedDraw = "Tue & Thu";
 	this.duration = "1";
 	this.durations = ["1", "2", "4", "8"];
 	this.defaultCurrencySymbol = "£";
+	this.dynPath = "css";
 
 });
 
-
-ticketApp.directive('ticketAnimate', function($timeout) {
+ticketApp.directive('ticketAnimate', function ($timeout) {
 	return {
 		restrict : 'A',
-		link: function(scope, element)
-		{
-			$timeout(function() {
+		link : function (scope, element) {
+			$timeout(function () {
 				$(element).addClass('slide');
 			}, 10);
 		}
 	};
 });
 
-ticketApp.directive('tapClick', function() {
+ticketApp.directive('tapClick', function () {
 	return {
 		restrict : 'EA',
-		link: function(scope, element)
-		{
-			$(element).on("TapClickTouchStart", 
-				function(event)
-				{
-					var method = element.attr("ng-click");
-					scope.$event = event;
-					scope.$apply(method);
-				}
-			);
+		link : function (scope, element) {
+			$(element).on("TapClickTouchStart",
+				function (event) {
+				var method = element.attr("ng-click");
+				scope.$event = event;
+				scope.$apply(method);
+			});
 		}
 	};
 });
-
-
